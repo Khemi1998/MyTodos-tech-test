dea2480bff723ac9f37198cdfdb24c2e37b67c45
Khemi <khemi@Khemis-Laptop.local>
2021-11-24 20:27:56 +0000

    iteration8: fixing button position

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -4,17 +4,16 @@ var input = document.querySelector(".toDo__input");
 var add = document.querySelector(".toDo__add");
 var tasks = document.querySelector(".toDo_listings");
 var listStatus = document.querySelector(".toDo__status");
-var reset = document.querySelector("#reset"); //adding tasks
+var reset = document.querySelector("#reset");
+var deletingStatus = document.querySelector(".toDo__deletion"); //adding tasks
 
 add.addEventListener("click", function () {
   //needs to have text to be a task
-  var inputText = input.value;
-
-  if (inputText.length > 0) {
+  if (input.value.length > 0) {
     listStatus.innerHTML = "Tasks to complete: ".concat(tasks.childNodes.length + 1);
     var task = document.createElement("div");
     tasks.appendChild(task);
-    task.innerHTML = "".concat(inputText); //check box
+    task.innerHTML = "".concat(input.value); //add check box
 
     var check = document.createElement("input");
     check.setAttribute("type", "checkbox");
@@ -25,4 +24,9 @@ add.addEventListener("click", function () {
 reset.addEventListener("click", function () {
   listStatus.innerHTML = "All tasks have been completed";
   tasks.innerHTML = "";
+  input.value = "";
 });
+/*
+    deletingStatus.style.display = "flex";
+    deletingStatus.innerHTML = ""
+    */
--- main.js
+++ main.js
@@ -3,21 +3,21 @@ const add = document.querySelector(".toDo__add");
 const tasks = document.querySelector(".toDo_listings");
 const listStatus = document.querySelector(".toDo__status");
 const reset = document.querySelector("#reset");
+const deletingStatus = document.querySelector(".toDo__deletion");
 
 //adding tasks
 add.addEventListener("click", ()=>{
-
 //needs to have text to be a task
-const inputText = input.value;
 
-if (inputText.length>0) {
+if (input.value.length>0) {
     listStatus.innerHTML = `Tasks to complete: ${tasks.childNodes.length+1}`;
 
     const task = document.createElement("div");
     tasks.appendChild(task);    
-    task.innerHTML=`${inputText}`;
-    //check box
+    task.innerHTML=`<h3>${input.value}</h3>`;
+    input.value = "";
     
+    //add check box
     const check =document.createElement("input");
     check.setAttribute("type", "checkbox");
     task.appendChild(check);
@@ -27,7 +27,21 @@ if (inputText.length>0) {
 //reset
 reset.addEventListener("click", ()=> {
     listStatus.innerHTML = `All tasks have been completed`;
-
     tasks.innerHTML = "";
+    input.value = "";
 })
 
+const checked = document.querySelectorAll('input[type="checkbox"]:checked');
+checked.forEach((button)=>{
+    alert(document.querySelectorAll('input[type="checkbox"]:checked').length);
+    button.addEventListener("click",(event)=>{
+        
+    alert("hi")
+        
+    })
+})
+
+/*
+    deletingStatus.style.display = "block";
+    deletingStatus.innerHTML = ``
+    */
